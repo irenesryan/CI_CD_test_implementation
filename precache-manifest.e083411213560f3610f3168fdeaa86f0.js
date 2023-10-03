344d9a021c3344d327c5e23a8bafd7bf501b7dac
irenesryan <irene.ryan@arkonit.se>
2023-10-03 06:58:11 +0000

    Updates

---
+++ precache-manifest.e083411213560f3610f3168fdeaa86f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7366cbfde7ffa1ce9176",
+    "url": "/CI_CD_test_implementation/static/css/main.c70a0f35.chunk.css"
+  },
+  {
+    "revision": "7366cbfde7ffa1ce9176",
+    "url": "/CI_CD_test_implementation/static/js/main.7366cbfd.chunk.js"
+  },
+  {
+    "revision": "3c11774c00c8fe3168dd",
+    "url": "/CI_CD_test_implementation/static/js/1.3c11774c.chunk.js"
+  },
+  {
+    "revision": "6422faee7361a7d7a3d5",
+    "url": "/CI_CD_test_implementation/static/js/2.6422faee.chunk.js"
+  },
+  {
+    "revision": "fa359ff0292430bafc71",
+    "url": "/CI_CD_test_implementation/static/js/runtime~main.fa359ff0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/CI_CD_test_implementation/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "10a34319e56666c5eec82a399c700125",
+    "url": "/CI_CD_test_implementation/index.html"
+  }
+];
